b8e950ebe7ab2eb538a11cc9265e2e10f1d0d7b2
pravinshambharkar <pravinshambharkar01@gmail.com>
2025-09-07 19:02:50 +0530

    changes

---
+++ CrimePortal/Controllers/CrimeController.cs
@@ -0,0 +1,18 @@
+﻿using CrimePortal.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrimePortal.Controllers
+{
+    public class CrimeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Crime> crimes = new List<Crime>
+            {
+                new Crime{}
+            };
+            
+            return View(crimes);
+        }
+    }
+}
---
+++ CrimePortal/ViewModels/Crime.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrimePortal.ViewModels
+{
+    public class Crime
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "गु र क्र व दिनांक")]
+        public string CrimeNoDate { get; set; }
+
+        [Display(Name = "गुन्ह्याचे ठिकाण")]
+        public string Place { get; set; }
+
+        [Display(Name = "फिर्यादी")]
+        public string Complainant { get; set; }
+
+        [Display(Name = "आरोपी व पत्ता")]
+        public string AccusedAddress { get; set; }
+
+        [Display(Name = "अटक तारीख व वेळ")]
+        public DateTime? ArrestDate { get; set; }
+
+        [Display(Name = "पंचनामा तारीख व वेळ")]
+        public DateTime? PanchnamaDate { get; set; }
+
+        [Display(Name = "जप्त मुद्देमाल थोडक्यात वर्णन")]
+        public string SeizedItems { get; set; }
+
+        [Display(Name = "मुद्देमाल किंमत")]
+        public decimal? SeizedValue { get; set; }
+
+        [Display(Name = "साक्षिदारांची नावे व पत्ता")]
+        public string Witnesses { get; set; }
+
+        [Display(Name = "गुन्हा नोदविणाऱ्या अधिकाऱ्याचे नाव व पद")]
+        public string OfficerName { get; set; }
+    }
+}
